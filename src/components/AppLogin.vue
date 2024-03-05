ba8c406c7446f7ef2b713e2aebc91cc074284ce6
francoisxavierdesaintjean <francois-xavier1.leprovost-de-saint-jean@supinfo.com>
2024-03-05 09:26:54 +0100

    finish login link with graphql

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -5,9 +5,7 @@
       <h3 class="text-center mb-4">Connexion</h3>
       <p class="text-danger" v-if="errors.length">
         <b>Please correct the following error(s):</b>
-        <ul class="mb-0 list-unstyled">
-          <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
-        </ul>
+        <b v-for="(error, index) in errors" :key="index">{{ error }}</b>
       </p>
       <div class="form-floating mb-3">
         <input type="text" class="form-control" id="username" placeholder="Votre pseudo" v-model="username">
@@ -16,6 +14,12 @@
       <div class="form-floating mb-3">
         <input type="password" class="form-control" id="password" placeholder="Password" v-model="password">
         <label for="floatingPassword">Mot de passe</label>
+      </div>
+      <div class="form-check mb-3">
+        <input class="form-check-input" type="checkbox" value="" id="rememberPassword" v-model="rememberPassword">
+        <label class="form-check-label" for="rememberPassword">
+          Se souvenir de moi
+        </label>
       </div>
       <small class="text-body-secondary">En cliquant sur Se connecter, vous acceptez les conditions
         d'utilisation.</small>
@@ -40,6 +44,7 @@ export default {
     return {
       username: '',
       password: '',
+      rememberPassword: false,
       errors: [],
     };
   },
@@ -48,37 +53,36 @@ export default {
     async login() {
       event.preventDefault();
 
-      console.log('Données saisies :', this.email, this.password);
+      console.log('Données saisies :', this.username, this.password);
       const response = await fetch('http://localhost:3000/graphql', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          query: `mutation CreateUser {
-    createUser(
-        appUserInsertDto: {
-            userName: "${this.pseudo}"
-            email: "${this.email}"
-            password: "${this.password}"
-        }
-    ) {
-        balance
-        id
-        userName
-        email
-    }
+          query: `mutation{
+   signIn(appUserLoginDto: {
+    username:"${this.username}",
+    password:"${this.password}",
+    rememberMe:${this.rememberPassword}
+
+
+   })
+   {
+    succeeded,
+    isNotAllowed,
+   }
 }
 `
         })
 
       });
       const responseData = await response.json();
-      if (responseData.errors) {
-        this.errors = ['Erreur : ' + responseData.errors[0].message];
+      if (!responseData.data.signIn.succeeded) {
+        this.errors = ['Vérifiez vos informations de connexion'];
 
       }
-      if (responseData.data.createUser !== null) {
+      if (responseData.data.signIn.succeeded) {
         alert('Utilisateur connecté avec succès');
       }
     }
